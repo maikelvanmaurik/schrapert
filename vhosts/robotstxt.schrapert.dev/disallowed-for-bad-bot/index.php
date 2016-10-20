625838c2b6fe27d4f01013d170fe716572b52b5e
Maikel van Maurik <maikelvanmaurik@gmail.com>
2016-10-20 23:48:23 +0200

    Remove debug code

--- vhosts/robotstxt.schrapert.dev/disallowed-for-bad-bot/index.php
+++ vhosts/robotstxt.schrapert.dev/disallowed-for-bad-bot/index.php
@@ -1,3 +1,1 @@
-<h2>This page should not be crawled by bad bot</h2>
-<?php
-file_put_contents(__DIR__.'/test.txt', $_SERVER['HTTP_USER_AGENT'], FILE_APPEND);
+<h2>This page should not be crawled by BadBot</h2>
